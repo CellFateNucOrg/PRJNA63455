03e4e54ccb225e753c3e55bc3f1fe2cb306509b4
meisterpeter <peter.meister@izb.unibe.ch>
2020-06-24 09:54:17 +0200

    00 and looper modifications

--- 00_download.sh
+++ 00_download.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-module add UHTS/Analysis/sratoolkit/2.9.6.1;
+module add UHTS/Analysis/sratoolkit/2.10.7;
 
 #$1 is the full filename (with directory location) of the csv file with the ChIP set SRR info
 #$2 is the SRR numbers of the IP dataset to map
@@ -15,9 +15,13 @@ echo $3
 [ ! -d $SRR_exp/SRR_download/IP ] && mkdir $SRR_exp/SRR_download/IP
 [ ! -d $SRR_exp/SRR_download/input ] && mkdir $SRR_exp/SRR_download/input
 echo "Downloading IP: $SRR_IP"
-fastq-dump -gzip -O $SRR_exp/SRR_download/IP $SRR_IP
+prefetch -O $SRR_exp/SRR_download/IP/ -o $SRR_IP.srr $SRR_IP
+fasterq-dump -O $SRR_exp/SRR_download/IP -t ./tmp/ $SRR_IP.srr
+#fastq-dump -gzip -O $SRR_exp/SRR_download/IP $SRR_IP
 echo "Downloading input: $SRR_input"
-fastq-dump -gzip -O $SRR_exp/SRR_download/input $SRR_input
+prefetch -O $SRR_exp/SRR_download/input -o $SRR_input.srr $SRR_input
+fasterq-dump -O $SRR_exp/SRR_download/input -t ./tmp/ $SRR_input.srr
+#fastq-dump -gzip -O $SRR_exp/SRR_download/input $SRR_input
 echo "This is over"
 
-module rm UHTS/Analysis/sratoolkit/2.9.6.1;
+module rm UHTS/Analysis/sratoolkit/2.10.7;
--- looper.sh
+++ looper.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 #SBATCH --mail-user=peter.meister@izb.unibe.ch
 #SBATCH --mail-type=end,fail
+#SBATCH --array=3-99%10
 #SBATCH --job-name="ChIP_seq_modENCODE"
 #SBATCH --time=2-12:00:00
 #SBATCH --cpus-per-task=4
-#SBATCH --partition=all
 #SBATCH --mem-per-cpu=8G
 
-for i in {2..17}
-do
-    echo "Line $i"
-    bash wrapper.sh SRR_names.csv $i
-done
-
-echo "This is really over"
+echo "Line $SLURM_ARRAY_TASK_ID"
+sh wrapper.sh SRR_names.csv $SLURM_ARRAY_TASK_ID
 
 
